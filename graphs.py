391fac7d799cce68d3a009f7ebd39996f7ea4e6e
padobrik <fkabachenko@gmail.com>
2022-12-06 18:52:38 +0300

    Added static methods for Graph class

--- graphs.py
+++ graphs.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Union, Iterator
 
 # Класс Graph работает с объектами, выступающими словарем:
 # Ключ = вершина
@@ -18,15 +18,19 @@ class Graph(object):
     
     def edges(self, vertice: List[str]) -> List[str]:
         '''
-        Возвращает список всех ребер графа относительно вершин
+        Возвращает список всех ребер графа относительно вершины
         '''
         return self._graph_dict[vertice]
     
-    def add_edge(self, edge):
+    def add_edge(self, edge: Union[set, list, tuple]):
         '''
         Предусматриваем, что ребро может быть подано в качестве
         параметра через типы list, set или tuple. При этом между
         вершинами может быть несколько ребер
+
+        Если хотим использовать направленный граф, то нужно
+        работать со списками ребер, ибо множество является
+        неупорядоченным типом данных 
         '''
         edge = set(edge)
         vertex_1, vertex_2 = tuple(edge)
@@ -36,6 +40,23 @@ class Graph(object):
                 self._graph_dict[x].add(y)
             else:
                 self._graph_dict[x] = [y]
+    
+    def all_edges(self) -> List[set]:
+        '''
+        Возвращает все ребра графа
+        '''
+        return self.__generate_edges()
+    
+    def __generate_edges(self) -> List[set]:
+        '''
+        Статический метод генерации ребер графа
+        '''
+        edges = []
+        for vertex in self._graph_dict:
+            for neighbour in self._graph_dict[vertex]:
+                if {neighbour, vertex} not in edges:
+                    edges.append({vertex, neighbour})
+        return edges
         
     def vertices(self) -> set:
         '''
@@ -49,6 +70,29 @@ class Graph(object):
         ключ vertex с пустым списком в словарь
         '''    
         if vertex not in self._graph_dict.keys():
-            self.graph_dict[vertex] = []    
+            self.graph_dict[vertex] = []   
 
+    # Некоторые статические методы для полного описания класса:
+    def __str__(self) -> str:
+        '''
+        Метолд для описания объекта класса в виде строки
+        '''
+        res = 'Vertices: '
+        for k in self._graph_dict:
+            res += str(k) + ' '
+        res += '\nEdges: '
+        for edge in self.__generate_edges():
+            res += str(edge) + ' '
+        return res
+
+    def __iter__(self) -> Iterator:
+        '''
+        Возвращает итерируемый объект
+        '''
+        self._iter_obj = iter(self._graph_dict)
+        return self._iter_obj
     
+    def __next__(self) -> Iterator:
+        '''
+        Для прохождения по вершинам графа
+        '''
